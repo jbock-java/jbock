17e2be043cbb62b3c63de9ffee39b8968f9aeda0
h908714124 <kraftdurchblumen@gmx.de>
2017-11-15 17:03:49 +0100

    add javadoc

--- core/src/main/java/net/jbock/LongName.java
+++ core/src/main/java/net/jbock/LongName.java
@@ -5,6 +5,22 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * <p>
+ *   This annotation is used by the jbock annotation processor.
+ * </p>
+ *
+ * <p>
+ *   The annotated method must be abstract and have
+ *   an empty argument list.
+ * </p>
+ *
+ * <p>
+ *   The enclosing class must be annotated with {@link CommandLineArguments}.
+ * </p>
+ *
+ * @see <a href="https://github.com/h908714124/jbock">jbock on github</a>
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.SOURCE)
 public @interface LongName {
