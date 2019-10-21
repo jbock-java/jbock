3a3fd25f6db1d6dd9fbc84ec35f8b53f7c47ba89
h908714124 <kraftdurchblumen@gmx.de>
2019-10-21 10:58:40 +0200

    refactoring

--- core/src/main/java/net/jbock/coerce/either/Either.java
+++ core/src/main/java/net/jbock/coerce/either/Either.java
@@ -14,16 +14,16 @@ public abstract class Either<L, R> {
 
   public <L2, R2> Either<L2, R2> map(Function<L, L2> leftFunction, Function<R, R2> rightFunction) {
     if (this instanceof Left) {
-      return Either.left(leftFunction.apply(((Left<L, R>) this).value()));
+      return left(leftFunction.apply(((Left<L, R>) this).value()));
     }
-    return Either.right(rightFunction.apply(((Right<L, R>) this).value()));
+    return right(rightFunction.apply(((Right<L, R>) this).value()));
   }
 
   public <R2> Either<L, R2> map(Function<R, Either<L, R2>> rightFunction) {
-    if (this instanceof Right) {
-      return rightFunction.apply(((Right<L, R>) this).value());
+    if (this instanceof Left) {
+      return left(((Left<L, R>) this).value());
     }
-    return Either.left(((Left<L, R>) this).value());
+    return rightFunction.apply(((Right<L, R>) this).value());
   }
 
   public R orElseThrow(Function<L, ? extends RuntimeException> f) {
