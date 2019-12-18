1a8a92fc2a1190a8368597e8a5777d51925c6815
h908714124 <kraftdurchblumen@gmx.de>
2019-12-18 16:41:11 +0100

    refactoring

--- core/src/main/java/net/jbock/coerce/MapperClassValidator.java
+++ core/src/main/java/net/jbock/coerce/MapperClassValidator.java
@@ -32,34 +32,26 @@ public final class MapperClassValidator {
   public Either<String, ReferenceMapperType> checkReturnType() {
     commonChecks(mapperClass);
     checkNotAbstract(mapperClass);
-    ReferencedType<Function> functionType = new ReferenceTool<>(MAPPER, basicInfo, mapperClass)
-        .getReferencedType();
+    ReferencedType<Function> functionType = new ReferenceTool<>(MAPPER, basicInfo, mapperClass).getReferencedType();
     TypeMirror t = functionType.typeArguments().get(0);
     TypeMirror r = functionType.typeArguments().get(1);
-    return tool().unify(tool().asType(String.class), t)
-        .flatMap(MAPPER::boom,
-            t_result -> handleTResult(functionType, r, t_result));
+    return tool().unify(tool().asType(String.class), t).flatMap(MAPPER::boom, t_result ->
+        handle(functionType, r, t_result));
   }
 
-  private Either<String, ReferenceMapperType> handleTResult(ReferencedType<Function> functionType, TypeMirror r, Map<String, TypeMirror> t_result) {
-    return tool().unify(expectedReturnType, r)
-        .flatMap(MAPPER::boom,
-            r_result -> handleRResult(functionType, t_result, r_result));
+  private Either<String, ReferenceMapperType> handle(ReferencedType<Function> functionType, TypeMirror r, Map<String, TypeMirror> t_result) {
+    return tool().unify(expectedReturnType, r).flatMap(MAPPER::boom, r_result ->
+        handle(functionType, t_result, r_result));
   }
 
-  private Either<String, ReferenceMapperType> handleRResult(ReferencedType<Function> functionType, Map<String, TypeMirror> t_result, Map<String, TypeMirror> r_result) {
+  private Either<String, ReferenceMapperType> handle(ReferencedType<Function> functionType, Map<String, TypeMirror> t_result, Map<String, TypeMirror> r_result) {
     return new Flattener(basicInfo, mapperClass)
         .getTypeParameters(Arrays.asList(t_result, r_result))
-        .map(MAPPER::boom,
-            typeParameters -> MapperType.create(tool(), functionType.isSupplier(),
-                mapperClass, typeParameters));
+        .map(MAPPER::boom, typeParameters ->
+            MapperType.create(tool(), functionType.isSupplier(), mapperClass, typeParameters));
   }
 
   private TypeTool tool() {
     return basicInfo.tool();
-  }
-
-  private Either<String, ReferenceMapperType> failure(String message) {
-    return Either.left(MAPPER.boom(message));
   }
 }
