a9323679083872dbb466a768b1fed5473e928c56
h908714124 <kraftdurchblumen@gmx.de>
2021-05-25 10:11:53 +0200

    add 4 dagger scopes

---
+++ core/src/main/java/net/jbock/scope/ParameterScope.java
@@ -0,0 +1,11 @@
+package net.jbock.scope;
+
+import javax.inject.Scope;
+import java.lang.annotation.Retention;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Scope
+@Retention(RUNTIME)
+public @interface ParameterScope {
+}
