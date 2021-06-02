c3c06f19921e350233315e9140b8cfd8287813ac
h908714124 <kraftdurchblumen@gmx.de>
2021-06-02 13:21:05 +0200

    test cleanup

--- examples/src/test/java/net/jbock/examples/AtFileTest.java
+++ examples/src/test/java/net/jbock/examples/AtFileTest.java
@@ -1,37 +1,46 @@
 package net.jbock.examples;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AtFileTest {
 
+  private final __AdditionArguments_Parser_AtFileReader reader =
+      new __AdditionArguments_Parser_AtFileReader();
+
   @Test
-  void testAtFileSyntax() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-    List<String> lines = new ArrayList<>();
-    lines.add("");
-    lines.add("1");
-    lines.add("");
-    lines.add("2\\\"\\ \\\\3\\");
-    lines.add("  4 ");
-    lines.add("");
-    lines.add("");
-    lines.add("");
-    lines.add("");
-    __AdditionArguments_Parser_AtFileReader reader = new __AdditionArguments_Parser_AtFileReader();
-    Method readAtLines = reader.getClass().getDeclaredMethod("readAtLines", List.class);
-    readAtLines.setAccessible(true);
-    @SuppressWarnings("unchecked")
-    List<String> tokens = (List<String>) readAtLines.invoke(reader, lines);
-    assertEquals(4, tokens.size());
-    assertEquals("", tokens.get(0));
-    assertEquals("1", tokens.get(1));
-    assertEquals("", tokens.get(2));
-    assertEquals("2\" \\3  4 ", tokens.get(3));
+  void testAtFileSyntax() {
+    List<String> lines = List.of(
+        "",
+        "1",
+        "",
+        "2\\\"\\ \\\\3\\",
+        "  4 ",
+        "",
+        "",
+        "",
+        "");
+    List<String> tokens = invokeReadAtLines(lines);
+    Assertions.assertEquals(List.of(
+        "",
+        "1",
+        "",
+        "2\" \\3  4 "),
+        tokens);
+  }
+
+  private List<String> invokeReadAtLines(List<String> lines) {
+    try {
+      Method readAtLines = reader.getClass().getDeclaredMethod("readAtLines", List.class);
+      readAtLines.setAccessible(true);
+      @SuppressWarnings("unchecked")
+      List<String> tokens = (List<String>) readAtLines.invoke(reader, lines);
+      return tokens;
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
   }
 }
