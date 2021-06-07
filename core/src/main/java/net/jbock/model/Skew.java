2f54274cfdc7ece81585b30d22b69dca9ab95036
h908714124 <kraftdurchblumen@gmx.de>
2021-06-07 22:07:28 +0200

    add javadoc

--- core/src/main/java/net/jbock/model/Skew.java
+++ core/src/main/java/net/jbock/model/Skew.java
@@ -2,9 +2,34 @@ package net.jbock.model;
 
 import java.util.Locale;
 
+/**
+ * An important property of an {@link Item}.
+ */
 public enum Skew {
 
-  REQUIRED, OPTIONAL, REPEATABLE, MODAL_FLAG;
+  /**
+   * The item is required.
+   * It must be present in the input array.
+   */
+  REQUIRED,
+
+  /**
+   * The item is optional.
+   * It may or may not be present in the input array.
+   */
+  OPTIONAL,
+
+  /**
+   * The item is repeatable.
+   */
+  REPEATABLE,
+
+  /**
+   * The item is a modal flag.
+   * A {@link Parameter} can't be a flag,
+   * so the item must be an {@link Option}.
+   */
+  MODAL_FLAG;
 
   @Override
   public String toString() {
