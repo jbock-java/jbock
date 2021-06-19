89a04185b58495a0f4909052066038be9d9cca16
h908714124 <kraftdurchblumen@gmx.de>
2021-06-19 15:47:20 +0200

    test refactoring

--- examples/src/test/java/net/jbock/examples/HelplessArgumentsTest.java
+++ examples/src/test/java/net/jbock/examples/HelplessArgumentsTest.java
@@ -1,12 +1,7 @@
 package net.jbock.examples;
 
 import net.jbock.examples.fixture.ParserTestFixture;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class HelplessArgumentsTest {
 
@@ -17,40 +12,33 @@ class HelplessArgumentsTest {
 
   @Test
   void success0() {
-    HelplessArguments args = parser.parse(new String[]{"x"})
-        .orElseThrow(notSuccess -> Assertions.<RuntimeException>fail("expecting success but was " + notSuccess));
-    assertEquals("x", args.required());
-    assertFalse(args.help());
+    f.assertThat("x").succeeds(
+        "required", "x",
+        "help", false);
   }
 
   @Test
   void success1() {
-    HelplessArguments args = parser.parse(new String[]{"x", "--help"})
-        .orElseThrow(notSuccess -> Assertions.<RuntimeException>fail("expecting success but was " + notSuccess));
-    assertTrue(args.help());
-    assertEquals("x", args.required());
+    f.assertThat("x", "--help").succeeds(
+        "required", "x",
+        "help", true);
   }
 
   @Test
   void success2() {
-    HelplessArguments args = parser.parse(new String[]{"--help", "x"})
-        .orElseThrow(notSuccess -> Assertions.<RuntimeException>fail("expecting success but was " + notSuccess));
-    assertTrue(args.help());
-    assertEquals("x", args.required());
+    f.assertThat("--help", "x").succeeds(
+        "required", "x",
+        "help", true);
   }
 
   @Test
   void errorNoArguments() {
     String[] emptyInput = new String[0];
-    assertTrue(parser.parse(emptyInput).getLeft().map(f::castToError)
-        .orElseThrow().message()
-        .contains("Missing required parameter REQUIRED"));
+    f.assertThat(emptyInput).fails("Missing required parameter REQUIRED");
   }
 
   @Test
   void errorInvalidOption() {
-    assertTrue(parser.parse("-p").getLeft().map(f::castToError)
-        .orElseThrow().message()
-        .contains("Invalid option: -p"));
+    f.assertThat("-p").fails("Invalid option: -p");
   }
 }
