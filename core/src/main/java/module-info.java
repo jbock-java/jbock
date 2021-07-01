403a58c4feb6f2f6d321dd9f543b027405fbfa86
h908714124 <kraftdurchblumen@gmx.de>
2021-07-01 23:18:14 +0200

    remove transitive module java.compiler

--- core/src/main/java/module-info.java
+++ core/src/main/java/module-info.java
@@ -1,12 +1,10 @@
 /**
  * <p>jbock runtime package: annotations and API.
  * The generated parser is annotated with
- * {@link javax.annotation.processing.Generated @Generated},
- * so {@code java.compiler} is a required module.</p>
+ * {@code javax.annotation.processing.Generated},
+ * so {@code java.compiler} is also required.</p>
  */
 module net.jbock {
-
-  requires transitive java.compiler;
 
   exports net.jbock;
   exports net.jbock.either;
