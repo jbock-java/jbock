6733135cf05ebab26b72817c8362c1bb7402714d
h908714124 <kraftdurchblumen@gmx.de>
2021-07-08 10:23:22 +0200

    javadoc

--- compiler/src/main/java/net/jbock/processor/ProcessorScope.java
+++ compiler/src/main/java/net/jbock/processor/ProcessorScope.java
@@ -5,6 +5,11 @@ import java.lang.annotation.Retention;
 
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+/**
+ * Initial processor scope,
+ * contains {@link javax.lang.model.util.Elements Elements}
+ * and {@link javax.lang.model.util.Types Types}.
+ */
 @Scope
 @Retention(RUNTIME)
 @interface ProcessorScope {
