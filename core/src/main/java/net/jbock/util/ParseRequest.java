e047d7499f1cb84eb0dc00bd70544c7812870499
h908714124 <kraftdurchblumen@gmx.de>
2021-08-19 14:07:14 +0200

    refactoring

--- core/src/main/java/net/jbock/util/ParseRequest.java
+++ core/src/main/java/net/jbock/util/ParseRequest.java
@@ -2,20 +2,21 @@ package net.jbock.util;
 
 import io.jbock.util.Either;
 
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Optional;
 
 /**
- * Input for the generated parser.
+ * A convenience class that performs {@code @-file} expansion,
+ * if the first token in the command line input starts with an
+ * {@code "@"} character.
  */
 public abstract class ParseRequest {
 
     /**
      * Creates a {@code ParseRequest}.
-     * {@code @-file} expansion will be enabled if the input array is nonempty,
-     * and the first token starts with an {@code "@"} character.
+     * {@code @-file} expansion will be performed if the input array is nonempty,
+     * and the first token contains at least 2 characters, and starts with an
+     * {@code "@"} character.
      *
      * @param args command line input
      * @return a parse request
@@ -26,49 +27,17 @@ public abstract class ParseRequest {
                 && args[0].startsWith("@")) {
             String fileName = args[0].substring(1);
             List<String> rest = List.of(args).subList(1, args.length);
-            return expand(Paths.get(fileName), rest);
+            return new ParseRequestExpand(Paths.get(fileName), rest);
         }
-        return simple(List.of(args));
+        return new ParseRequestSimple(List.of(args));
     }
 
     /**
-     * Creates a {@code ParseRequest} that indicates {@code @-file} expansion should
-     * not be performed.
+     * Returns a Right containing the result of {@code @-file} expansion.
+     * If an error occurs during {@code @-file} reading, returns a Left containing
+     * a description of the error.
      *
-     * @param args command line input
-     * @return a parse request
+     * @return the result of {@code @-file} expansion
      */
-    public static ParseRequest simple(List<String> args) {
-        return new ParseRequestSimple(args);
-    }
-
-    /**
-     * Creates a {@code ParseRequest} that indicates {@code @-file} expansion should
-     * be performed.
-     *
-     * @param atFile the {@code @-file}
-     * @param rest additional command line arguments
-     * @return a parse request
-     */
-    public static ParseRequest expand(Path atFile, List<String> rest) {
-        return new ParseRequestExpand(atFile, rest);
-    }
-
-    /**
-     * Returns the path of the {@code @-file}, or an empty
-     * {@code Optional} if {@code @-file} expansion is
-     * not requested.
-     *
-     * @return path of the {@code @-file}, or {@code empty}
-     */
-    public abstract Optional<Path> path();
-
-    /**
-     * Returns the command line arguments, excluding the {@code @-file}.
-     *
-     * @return command line arguments
-     */
-    public abstract List<String> args();
-
     public abstract Either<? extends AtFileError, List<String>> expand();
 }
