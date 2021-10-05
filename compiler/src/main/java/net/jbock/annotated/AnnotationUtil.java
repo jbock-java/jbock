9c89a49d2986e63d9928d876d7bbab1b7b173dde
jbock <jbock-java@gmx.de>
2021-10-05 22:46:15 +0200

    refactoring

--- compiler/src/main/java/net/jbock/annotated/AnnotationUtil.java
+++ compiler/src/main/java/net/jbock/annotated/AnnotationUtil.java
@@ -1,33 +1,30 @@
 package net.jbock.annotated;
 
 import com.google.auto.common.AnnotationMirrors;
-import com.google.auto.common.MoreTypes;
-import net.jbock.Option;
-import net.jbock.Parameter;
-import net.jbock.Parameters;
+import net.jbock.common.Annotations;
 
 import javax.lang.model.element.AnnotationMirror;
-import javax.lang.model.element.AnnotationValue;
 import javax.lang.model.element.AnnotationValueVisitor;
 import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.Name;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.DeclaredType;
 import javax.lang.model.type.TypeMirror;
 import javax.lang.model.util.SimpleAnnotationValueVisitor9;
 import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toSet;
+import static net.jbock.common.TypeTool.AS_DECLARED;
+import static net.jbock.common.TypeTool.AS_TYPE_ELEMENT;
 
 final class AnnotationUtil {
 
     private static final String CONVERTER_ATTRIBUTE = "converter";
 
-    private static final Set<String> ANNOTATIONS = Stream.of(
-                    Parameter.class,
-                    Parameters.class,
-                    Option.class)
-            .map(Class::getCanonicalName).collect(toSet());
+    private final Set<String> methodLevelAnnotations = Annotations.methodLevelAnnotations().stream()
+            .map(Class::getCanonicalName)
+            .collect(toSet());
 
     // visible for testing
     static final AnnotationValueVisitor<Optional<TypeMirror>, Void> GET_TYPE = new SimpleAnnotationValueVisitor9<>() {
@@ -44,30 +41,23 @@ final class AnnotationUtil {
     };
 
     Optional<TypeElement> getConverterAttribute(ExecutableElement sourceMethod) {
-        return getAnnotationMirror(sourceMethod)
-                .map(this::getAnnotationValue)
+        return getMethodAnnotation(sourceMethod)
+                .map(mirror -> AnnotationMirrors.getAnnotationValue(mirror, CONVERTER_ATTRIBUTE))
                 .flatMap(GET_TYPE::visit)
-                .map(MoreTypes::asTypeElement)
-                .filter(this::isNotVoid);
+                .flatMap(AS_DECLARED::visit)
+                .map(DeclaredType::asElement)
+                .flatMap(AS_TYPE_ELEMENT::visit)
+                .filter(element -> !"java.lang.Void".equals(element.getQualifiedName().toString()));
     }
 
-    private Optional<AnnotationMirror> getAnnotationMirror(ExecutableElement sourceMethod) {
+    private Optional<AnnotationMirror> getMethodAnnotation(ExecutableElement sourceMethod) {
         return sourceMethod.getAnnotationMirrors().stream()
-                .filter(this::hasAnnotationTypeIn)
+                .filter(mirror -> AS_TYPE_ELEMENT.visit(mirror.getAnnotationType().asElement())
+                        .map(TypeElement::getQualifiedName)
+                        .map(Name::toString)
+                        .filter(methodLevelAnnotations::contains)
+                        .isPresent())
                 .map((AnnotationMirror a) -> a) // Avoid returning Optional<? extends AnnotationMirror>.
                 .findFirst();
-    }
-
-    private boolean hasAnnotationTypeIn(AnnotationMirror annotation) {
-        return ANNOTATIONS.contains(
-                MoreTypes.asTypeElement(annotation.getAnnotationType()).getQualifiedName().toString());
-    }
-
-    private AnnotationValue getAnnotationValue(AnnotationMirror annotationMirror) {
-        return AnnotationMirrors.getAnnotationValue(annotationMirror, CONVERTER_ATTRIBUTE);
-    }
-
-    private boolean isNotVoid(TypeElement typeElement) {
-        return !"java.lang.Void".equals(typeElement.getQualifiedName().toString());
     }
 }
