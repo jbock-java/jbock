fd6c0dff14c7392dabf1e738a86159f01ef5395f
jbock <jbock-java@gmx.de>
2021-10-07 10:50:10 +0200

    refactoring

--- compiler/src/main/java/net/jbock/annotated/AnnotationUtil.java
+++ compiler/src/main/java/net/jbock/annotated/AnnotationUtil.java
@@ -1,22 +1,20 @@
 package net.jbock.annotated;
 
-import com.google.auto.common.AnnotationMirrors;
-
 import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.AnnotationValue;
 import javax.lang.model.element.AnnotationValueVisitor;
+import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.DeclaredType;
 import javax.lang.model.type.TypeMirror;
 import javax.lang.model.util.SimpleAnnotationValueVisitor9;
+import java.util.Map;
 import java.util.Optional;
 
 import static net.jbock.common.TypeTool.AS_DECLARED;
 import static net.jbock.common.TypeTool.AS_TYPE_ELEMENT;
 
 final class AnnotationUtil {
-
-    private static final String CONVERTER_ATTRIBUTE = "converter";
 
     // visible for testing
     static final AnnotationValueVisitor<Optional<TypeMirror>, Void> GET_TYPE = new SimpleAnnotationValueVisitor9<>() {
@@ -33,8 +31,13 @@ final class AnnotationUtil {
     };
 
     Optional<TypeElement> getConverterAttribute(AnnotationMirror mirror) {
-        AnnotationValue converter = AnnotationMirrors.getAnnotationValue(mirror, CONVERTER_ATTRIBUTE);
-        return GET_TYPE.visit(converter)
+        Map<? extends ExecutableElement, ? extends AnnotationValue> elementValues =
+                mirror.getElementValues();
+        return elementValues.entrySet().stream()
+                .filter(e -> "converter".equals(e.getKey().getSimpleName().toString()))
+                .map(Map.Entry::getValue)
+                .findFirst()
+                .flatMap(GET_TYPE::visit)
                 .flatMap(AS_DECLARED::visit)
                 .map(DeclaredType::asElement)
                 .flatMap(AS_TYPE_ELEMENT::visit)
