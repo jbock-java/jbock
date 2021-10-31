667e8814051cb32bfa86aa5f2caf28c140ceecb2
jbock <jbock-java@gmx.de>
2021-10-31 07:39:27 +0100

    declare annotation processor in module-info

--- compiler/src/main/java/module-info.java
+++ compiler/src/main/java/module-info.java
@@ -1,5 +1,7 @@
 module net.jbock.compiler {
 
+    provides javax.annotation.processing.Processor with net.jbock.processor.JbockProcessor;
+
     requires java.compiler;
     requires com.squareup.javapoet;
     requires io.jbock.util;
