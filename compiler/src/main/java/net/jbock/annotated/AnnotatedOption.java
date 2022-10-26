d8f8cea8d4f5719b4a181a99ff5d0777d3de7f0f
jbock <jbock-java@gmx.de>
2022-10-26 21:32:35 +0200

    refactoring, move paramLabel to ExecutableOption

--- compiler/src/main/java/net/jbock/annotated/AnnotatedOption.java
+++ compiler/src/main/java/net/jbock/annotated/AnnotatedOption.java
@@ -1,35 +1,8 @@
 package net.jbock.annotated;
 
-import net.jbock.common.SnakeName;
-
-import java.util.Comparator;
 import java.util.List;
-import java.util.Locale;
-import java.util.function.Supplier;
-
-import static java.util.stream.Collectors.toList;
-import static net.jbock.common.Suppliers.memoize;
 
 public final class AnnotatedOption extends AnnotatedMethod<ExecutableOption> {
-
-    // visible for testing
-    static final Comparator<String> LENGTH_FIRST_COMPARATOR = Comparator
-            .comparing(String::length)
-            .thenComparing(String::toString);
-
-    private final Supplier<List<String>> names = memoize(() -> executable().names().stream()
-            .sorted(LENGTH_FIRST_COMPARATOR)
-            .collect(toList()));
-
-    private final Supplier<String> paramLabel = memoize(() -> executable().paramLabel()
-            .or(() -> names().stream()
-                    .filter(name -> name.startsWith("--"))
-                    .map(name -> name.substring(2))
-                    .map(s -> s.toUpperCase(Locale.ROOT))
-                    .findFirst())
-            .orElseGet(() -> SnakeName.create(executable().simpleName())
-                    .snake('_')
-                    .toUpperCase(Locale.ROOT)));
 
     private AnnotatedOption(ExecutableOption option) {
         super(option);
@@ -51,10 +24,10 @@ public final class AnnotatedOption extends AnnotatedMethod<ExecutableOption> {
 
     @Override
     public String paramLabel() {
-        return paramLabel.get();
+        return executable().paramLabel();
     }
 
     public List<String> names() {
-        return names.get();
+        return executable().names();
     }
 }
--- compiler/src/main/java/net/jbock/annotated/ExecutableOption.java
+++ compiler/src/main/java/net/jbock/annotated/ExecutableOption.java
@@ -1,16 +1,40 @@
 package net.jbock.annotated;
 
 import net.jbock.Option;
+import net.jbock.common.SnakeName;
 
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
+import java.util.function.Supplier;
 
+import static java.util.stream.Collectors.toList;
 import static net.jbock.annotated.AnnotatedOption.createOption;
 import static net.jbock.common.Constants.optionalString;
+import static net.jbock.common.Suppliers.memoize;
 
 final class ExecutableOption extends Executable {
+
+    private static final Comparator<String> LENGTH_FIRST_COMPARATOR = Comparator
+            .comparing(String::length)
+            .thenComparing(String::toString);
+
+    private final Supplier<List<String>> names = memoize(() -> optionNames().stream()
+            .sorted(LENGTH_FIRST_COMPARATOR)
+            .collect(toList()));
+
+    private final Supplier<String> paramLabel = memoize(() -> optionParamLabel()
+            .or(() -> optionNames().stream()
+                    .filter(name -> name.startsWith("--"))
+                    .map(name -> name.substring(2))
+                    .map(s -> s.toUpperCase(Locale.ROOT))
+                    .findFirst())
+            .orElseGet(() -> SnakeName.create(simpleName())
+                    .snake('_')
+                    .toUpperCase(Locale.ROOT)));
 
     private final Option option;
 
@@ -38,11 +62,19 @@ final class ExecutableOption extends Executable {
         return List.of(option.description());
     }
 
-    List<String> names() {
+    public List<String> names() {
+        return names.get();
+    }
+
+    public String paramLabel() {
+        return paramLabel.get();
+    }
+    
+    private List<String> optionNames() {
         return List.of(option.names());
     }
 
-    Optional<String> paramLabel() {
+    Optional<String> optionParamLabel() {
         return optionalString(option.paramLabel());
     }
 }
