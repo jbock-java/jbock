512481ba568ba94740c2a498aadf363a2408c6b2
jbock <jbock-java@gmx.de>
2022-10-29 12:08:59 +0200

    refactoring

--- compiler/src/main/java/net/jbock/common/Suppliers.java
+++ compiler/src/main/java/net/jbock/common/Suppliers.java
@@ -5,22 +5,18 @@ import java.util.function.Supplier;
 public final class Suppliers {
 
     public static <T> Supplier<T> memoize(Supplier<T> delegate) {
-        return Cache.of(delegate);
+        return new Cache<>(delegate);
     }
 
     private static final class Cache<T> implements Supplier<T> {
 
-        private boolean initialized;
-        private T value;
+        boolean initialized;
+        T value;
 
-        private final Supplier<T> delegate;
+        final Supplier<T> delegate;
 
         Cache(Supplier<T> delegate) {
             this.delegate = delegate;
-        }
-
-        static <T> Cache<T> of(Supplier<T> supplier) {
-            return new Cache<>(supplier);
         }
 
         @Override
