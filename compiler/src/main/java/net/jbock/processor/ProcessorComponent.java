52b1ac388dee64c1fcb8d6b64298a1ac2df5a282
jbock <jbock-java@gmx.de>
2023-01-11 13:21:19 +0100

    refactoring ProcessorComponent

--- compiler/src/main/java/net/jbock/processor/ProcessorComponent.java
+++ compiler/src/main/java/net/jbock/processor/ProcessorComponent.java
@@ -8,35 +8,29 @@ import net.jbock.common.Util;
 import javax.annotation.processing.Filer;
 import javax.annotation.processing.Messager;
 import javax.annotation.processing.ProcessingEnvironment;
-import java.util.function.Supplier;
-
-import static net.jbock.common.Suppliers.memoize;
 
 final class ProcessorComponent {
 
-    private final Supplier<Util> utilProvider;
-    private final Supplier<SafeTypes> typesProvider;
-    private final Supplier<SafeElements> elementsProvider;
-    private final Supplier<TypeTool> toolProvider;
-    private final Supplier<Filer> filerProvider;
-    private final Supplier<Messager> messagerProvider;
-    private final Supplier<SourceFileGenerator> sourceFileGeneratorProvider;
+    private final Util util;
+    private final TypeTool tool;
+    private final Messager messager;
+    private final SourceFileGenerator sourceFileGenerator;
 
     ProcessorComponent(ProcessingEnvironment processingEnvironment) {
-        this.typesProvider = memoize(() -> new SafeTypes(processingEnvironment.getTypeUtils()));
-        this.elementsProvider = memoize(() -> new SafeElements(processingEnvironment.getElementUtils()));
-        this.toolProvider = memoize(() -> new TypeTool(elementsProvider.get(), typesProvider.get()));
-        this.utilProvider = memoize(() -> new Util(typesProvider.get(), toolProvider.get()));
-        this.filerProvider = memoize(processingEnvironment::getFiler);
-        this.messagerProvider = memoize(processingEnvironment::getMessager);
-        this.sourceFileGeneratorProvider = memoize(() -> new SourceFileGenerator(filerProvider.get(), messagerProvider.get()));
+        SafeTypes types = new SafeTypes(processingEnvironment.getTypeUtils());
+        SafeElements elements = new SafeElements(processingEnvironment.getElementUtils());
+        Filer filer = processingEnvironment.getFiler();
+        this.tool = new TypeTool(elements, types);
+        this.util = new Util(types, tool);
+        this.messager = processingEnvironment.getMessager();
+        this.sourceFileGenerator = new SourceFileGenerator(filer, messager);
     }
 
     MethodStep methodStep() {
-        return new MethodStep(messagerProvider.get(), utilProvider.get());
+        return new MethodStep(messager, util);
     }
 
     CommandStep commandStep() {
-        return new CommandStep(messagerProvider.get(), utilProvider.get(), sourceFileGeneratorProvider.get(), toolProvider.get());
+        return new CommandStep(messager, util, sourceFileGenerator, tool);
     }
 }
